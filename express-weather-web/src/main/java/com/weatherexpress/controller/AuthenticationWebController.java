802b0932985bfb7a58be57a67bd048382edb9104
ashish kumar barik <ashish.kumar@kohls.com>
2019-05-30 11:48:02 +0530

    logging out the authenticated user successfully during logout process by ashish

--- express-weather-web/src/main/java/com/weatherexpress/controller/AuthenticationWebController.java
+++ express-weather-web/src/main/java/com/weatherexpress/controller/AuthenticationWebController.java
@@ -1,5 +1,13 @@
 package com.weatherexpress.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.http.HttpRequest;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,12 +26,17 @@ public class AuthenticationWebController {
 	}
 
 	@RequestMapping(value = "/logout")
-	public String logoutPage() {
+	public String logoutPage(HttpServletRequest req, HttpServletResponse res) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if (auth != null) {
+			new SecurityContextLogoutHandler().logout(req, res, auth);
+		}
 		return "customLoginPage";
 	}
 
-	@RequestMapping(value = "/userPost", method = RequestMethod.POST)
-	public String userPost() {
+	@RequestMapping(value = "/userPost")
+	public String userPost(HttpServletRequest req) {
+		System.out.println(req);
 		return "userPosts";
 	}
 }
